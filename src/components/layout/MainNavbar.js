51a7c9e090f72ff167990ea8fac016670e5d7377
Sasa Radovanovic <salle90ar@gmail.com>
2020-03-31 22:30:36 +0200

    Navbar customized

--- src/components/layout/MainNavbar.js
+++ src/components/layout/MainNavbar.js
@@ -1,37 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 const MainNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const collapse = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#link'>Link</Nav.Link>
-          <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.2'>
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href='#action/3.4'>
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-          <Button variant='outline-success'>Search</Button>
-        </Form>
-      </Navbar.Collapse>
+    <Navbar
+      id='main-navbar'
+      bg='dark'
+      variant='dark'
+      expand='sm'
+      expanded={expanded}
+    >
+      <Container>
+        <Navbar.Brand as={NavLink} to='/'>
+          Glozzom
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls='basic-navbar-nav'
+          onClick={() => setExpanded(expanded ? false : 'expanded')}
+        />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link
+              className='px-3'
+              as={NavLink}
+              exact
+              to='/'
+              onClick={collapse}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className='px-3'
+              as={NavLink}
+              exact
+              to='/about'
+              onClick={collapse}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              className='px-3'
+              as={NavLink}
+              exact
+              to='/services'
+              onClick={collapse}
+            >
+              Services
+            </Nav.Link>
+            <Nav.Link
+              className='px-3'
+              as={NavLink}
+              exact
+              to='/blog'
+              onClick={collapse}
+            >
+              Blog
+            </Nav.Link>
+            <Nav.Link
+              className='px-3'
+              as={NavLink}
+              exact
+              to='/contact'
+              onClick={collapse}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
