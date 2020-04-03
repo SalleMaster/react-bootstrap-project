dbc649467b0e4917d145f506e23b74e8d29dec15
Sasa Radovanovic <salle90ar@gmail.com>
2020-04-03 14:54:49 +0200

    Contact Page

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,7 +1,142 @@
 import React from 'react';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div id='blog'>
+      <header className='page-header'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 m-auto text-center'>
+              <h1>Read Our Blog</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia,
+                exercitationem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Blog Section */}
+      <section className='blog py-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div className='card'>
+                <img
+                  src='https://source.unsplash.com/random/300x200'
+                  alt=''
+                  className='img-fluid card-img-top'
+                />
+                <div className='card-body'>
+                  <h4 className='card-title'>Blog Post One</h4>
+                  <small className='text-muted'>Written by Jeff on 05/20</small>
+                  <hr />
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellendus, fuga odit ipsa, quae in suscipit temporibus
+                    inventore eveniet expedita numquam nam recusandae aliquid
+                    corrupti maxime natus saepe eos voluptate cumque.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card'>
+                <img
+                  src='https://source.unsplash.com/random/300x201'
+                  alt=''
+                  className='img-fluid card-img-top'
+                />
+                <div className='card-body'>
+                  <h4 className='card-title'>Blog Post Two</h4>
+                  <small className='text-muted'>Written by Jeff on 05/20</small>
+                  <hr />
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellendus, fuga odit ipsa, quae in suscipit temporibus
+                    inventore eveniet expedita numquam nam recusandae aliquid
+                    corrupti maxime natus saepe eos voluptate cumque.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card'>
+                <img
+                  src='https://source.unsplash.com/random/300x202'
+                  alt=''
+                  className='img-fluid card-img-top'
+                />
+                <div className='card-body'>
+                  <h4 className='card-title'>Blog Post Three</h4>
+                  <small className='text-muted'>Written by Jeff on 05/20</small>
+                  <hr />
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellendus, fuga odit ipsa, quae in suscipit temporibus
+                    inventore eveniet expedita numquam nam recusandae aliquid
+                    corrupti maxime natus saepe eos voluptate cumque.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card p-3'>
+                <div className='card-body'>
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod vero itaque dolores voluptatibus facilis sunt!
+                  </p>
+                  <blockquote className='blockquote-footer text-muted'>
+                    <small>
+                      Someone Famous in
+                      <br />
+                      <cite title='Source Title'>Source Title</cite>
+                    </small>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card p-3 bg-danger text-white'>
+                <div className='card-body'>
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod vero itaque dolores voluptatibus facilis sunt!
+                  </p>
+                  <blockquote className='blockquote-footer text-white'>
+                    <small>
+                      Someone Famous in
+                      <br />
+                      <cite title='Source Title'>Source Title</cite>
+                    </small>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card p-3'>
+                <div className='card-body'>
+                  <p className='card-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod vero itaque dolores voluptatibus facilis sunt!
+                  </p>
+                  <blockquote className='blockquote-footer text-muted'>
+                    <small>
+                      Someone Famous in
+                      <br />
+                      <cite title='Source Title'>Source Title</cite>
+                    </small>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,166 @@
 import React from 'react';
 
+import person1 from '../../img/person1.jpg';
+import person2 from '../../img/person2.jpg';
+import person3 from '../../img/person3.jpg';
+import person4 from '../../img/person4.jpg';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div id='contact'>
+      {/* Page Header */}
+      <header className='page-header'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 m-auto text-center'>
+              <h1>Contact Us</h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,
+                dolores.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Contact */}
+      <section className='contact py-3'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div className='card p-4'>
+                <h4>Get In Touch</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Accusantium, laborum.
+                </p>
+                <h4>Address</h4>
+                <p>550 Main st, Boston MA</p>
+                <h4>Email</h4>
+                <p>test@test.com</p>
+                <h4>Phone</h4>
+                <p>(555) 555-5555</p>
+              </div>
+            </div>
+            <div className='col-md-8'>
+              <div className='card p-4'>
+                <div className='card-body'>
+                  <h3 className='text-center'>
+                    Please fill out this form to cantact us
+                  </h3>
+                  <hr />
+                  <div className='row'>
+                    <div className='col-md-6'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='First Name'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-md-6'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='Last Name'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-md-6'>
+                      <div className='form-group'>
+                        <input
+                          type='email'
+                          className='form-control'
+                          placeholder='Email'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-md-6'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          className='form-control'
+                          placeholder='Phone Number'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-md-12'>
+                      <div className='form-group'>
+                        <textarea
+                          className='form-control'
+                          placeholder='Message'
+                          name=''
+                          id=''
+                        ></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col'>
+                      <button
+                        type='submit'
+                        className='btn btn-outline-danger btn-block'
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Staff */}
+      <section className='staff py-5 text-center bg-dark text-white'>
+        <div className='container'>
+          <h1>Our Staff</h1>
+          <hr />
+          <div className='row'>
+            <div className='col-md-3'>
+              <img
+                src={person1}
+                alt=''
+                className='img-fluid rounded-circle mb-2'
+              />
+              <h4>Jane Doe</h4>
+              <small>Marketing Manager</small>
+            </div>
+            <div className='col-md-3'>
+              <img
+                src={person2}
+                alt=''
+                className='img-fluid rounded-circle mb-2'
+              />
+              <h4>Sara Williams</h4>
+              <small>Business Manager</small>
+            </div>
+            <div className='col-md-3'>
+              <img
+                src={person3}
+                alt=''
+                className='img-fluid rounded-circle mb-2'
+              />
+              <h4>John Doe</h4>
+              <small>CEO</small>
+            </div>
+            <div className='col-md-3'>
+              <img
+                src={person4}
+                alt=''
+                className='img-fluid rounded-circle mb-2'
+              />
+              <h4>Steve Smith</h4>
+              <small>Web Developer</small>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
